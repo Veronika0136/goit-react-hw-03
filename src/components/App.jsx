6587e4db511c59f24ed8130b45b199109c02b8e4
veronika <artemova331@gmail.com>
2025-05-07 17:50:48 +0300

    create components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,17 @@
+import React from 'react';
+import ContactForm from '../components/ContactForm/ContactForm';
+import SearchBox from '../components/SearchBox/SearchBox';
+import ContactList from '../components/ContactList/ContactList';
 
+const App = () => {
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Contact from '../Contact/Contact';
+
+const ContactList = () => {
+  return (
+    <div>
+      ContactList
+      <Contact />
+    </div>
+  );
+};
+
+export default ContactList;
