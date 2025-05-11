12207286ab5a7e66c4b27a7632312ffc2cc7bcb0
veronika <artemova331@gmail.com>
2025-05-11 21:46:23 +0300

    add localStor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import initialContactList from '../assets/initialContactList.json';
 import { nanoid } from 'nanoid';
 import ContactForm from '../components/ContactForm/ContactForm';
 import SearchBox from '../components/SearchBox/SearchBox';
@@ -6,21 +7,18 @@ import ContactList from '../components/ContactList/ContactList';
 import s from '../components/App.module.css';
 
 const App = () => {
-  const initalContactList = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
-
-  const [contacts, setContacts] = useState(initalContactList);
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) || initialContactList
+  );
   const [values, setValues] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('dataContacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const handleChangeInput = e => {
     setValues(e.target.value);
   };
-
-  console.log(values);
 
   const handeleFilterContacts = (contacts, values) => {
     return contacts.filter(contact => contact.name.toLowerCase().includes(values.toLowerCase()));
@@ -28,20 +26,14 @@ const App = () => {
 
   const filterContacts = handeleFilterContacts(contacts, values);
 
-  console.log(filterContacts);
-
   const handleSubmit = (values, options) => {
     const newValues = { ...values, id: nanoid() };
-    console.log(newValues);
     setContacts(contacts => [...contacts, newValues]);
-    console.log(contacts);
 
     options.resetForm();
   };
 
   const handleDeleteContact = id => {
-    // const newContacts = contacts.filter(contact => contact.id !== id);
-    // console.log(newContacts);
     setContacts(contacts => {
       return contacts.filter(contact => contact.id !== id);
     });
